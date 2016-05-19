ece2a7ea9cbe4c1e7e3d9abd6c628e9510c904a3
christophevilleger <villeger.c@gmail.com>
2016-05-19 10:40:17 +0200

    init

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,27 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
+
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        return $this->render('default/index.html.twig');
+    }
+
+    public function searchAction(Request $request)
+    {
+        $q = $request->get('q');
+        if(empty($q)){
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/search.html.twig', [
+            'q' => $q
         ]);
     }
 }
