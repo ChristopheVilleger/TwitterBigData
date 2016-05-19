c43b66028a252327b5b12d3db0237a8926898c06
christophevilleger <villeger.c@gmail.com>
2016-05-19 12:57:51 +0200

    Add j7mbo/twitter-api-php and init

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use TwitterAPIExchange;
 
 class DefaultController extends Controller
 {
@@ -21,8 +22,25 @@ class DefaultController extends Controller
             return $this->redirectToRoute('homepage');
         }
 
+        $settings = array(
+            'oauth_access_token' => "630733550-nvIuANoKVx4XuY0NVuucosKhlpFfWLGZ2MVKoIXG",
+            'oauth_access_token_secret' => " O7wOcUs6pjgECPJ6ns532ARk0oAGo85MRceOArjWnlxCy",
+            'consumer_key' => "YjlJasQ7OLNyFgkGs1QSCmG70",
+            'consumer_secret' => "OfsKYPGmqnzEpKHuLUEetcSidcNrSltkW8E4cSZLNTieTA5ZGA"
+        );
+
+        $url = 'https://api.twitter.com/1.1/search/tweets.json';
+
+        $getfield = '?q=' . $q;
+        $requestMethod = 'GET';
+
+        $twitter = new TwitterAPIExchange($settings);
+        $resp = $twitter->setGetfield($getfield)
+            ->buildOauth($url, $requestMethod)
+            ->performRequest();
+
         return $this->render('default/search.html.twig', [
-            'q' => $q
+            'resp' => $resp
         ]);
     }
 }
